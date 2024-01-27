f8b2a3bc3fae81f39c9a70c504eff741e6b5b2b3
AlkerMalware <95124847+AlkerMalware@users.noreply.github.com>
2024-01-27 16:06:25 +0800

    Add files via upload

---
+++ Js-Test.js
@@ -0,0 +1,11 @@
+var button = document.getElementById('demo')
+var TexttBC = document.getElementsByClassName('option2')
+
+
+function changeText() {
+    button.textContent = "You are Correct!";
+}
+
+function changeTextW() {
+    button.textContent = "You are Wrong!";
+}
